b007feaf71cefb87038254b06eea6d12f8c7dcc5
Tyler Griffith <griffitht16@gmail.com>
2022-12-08 16:35:36 -0600

    Created Delete Functionalitity

--- songs/views.py
+++ songs/views.py
@@ -18,18 +18,20 @@ def songs_list(request):
         serializer.save()
         return Response(serializer.data, status=201)
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def song_detail(request, pk):
-    if request.method == 'GET':
-        song = get_object_or_404(Song, pk=pk)
+    song = get_object_or_404(Song, pk=pk)
+    if request.method == 'GET':        
         serializer = SongSerializer(song)
         return Response(serializer.data)
-    elif request.method == 'PUT':
-        song = get_object_or_404(Song, pk=pk)
+    elif request.method == 'PUT':    
         serializer = SongSerializer(song, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=204)
 
 
 
